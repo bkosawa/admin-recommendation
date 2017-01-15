8a9fc94d0eb4837044a3caa2387fccd82880307f
bruno.kosawa <bkosawa@gmail.com>
2017-01-15 16:16:14 -0200

    Implementing on classify command a comparison of similarity to test the AppClassifier

--- crawler/management/commands/classify.py
+++ crawler/management/commands/classify.py
@@ -7,12 +7,28 @@ from crawler.tasks import AppClassifier
 class Command(BaseCommand):
     help = 'Process similar csv'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--apps_count', type=int, default=-1,
+                            help='Number of apps to be classified. Default value is all')
+
     def handle(self, *args, **options):
+        apps_count = options['apps_count']
+
         category_keys = self.get_category_dict(Category.get_all_categories())
         developer_list = self.get_developer_dict(Developer.get_developer_list(), len(category_keys))
-        apps = App.objects.all()[:100]
+        if apps_count < 0:
+            apps = App.objects.all()
+        else:
+            apps = App.objects.all()[:apps_count]
         classifier = AppClassifier(apps, category_keys, developer_list)
-        classifier.create_utility_matrix()
+        matrix = classifier.create_utility_matrix()
+
+        for i in range(0, apps_count - 1):
+            for j in range(i + 1, apps_count):
+                row = matrix.getrow(i)
+                other_row = matrix.getrow(j)
+                if classifier.is_similar(row, other_row):
+                    print '{} and {} are similar'.format(apps[i].name(), apps[j].name())
 
     @staticmethod
     def get_category_dict(category_list, start=0):
