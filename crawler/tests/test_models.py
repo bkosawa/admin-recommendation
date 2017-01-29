e020870a275bb9caacc679f0fcb812a017f15dfc
bruno.kosawa <bkosawa@gmail.com>
2017-01-29 11:26:58 -0200

    Adding test to the case when there is a an empty array

--- crawler/tests/test_models.py
+++ crawler/tests/test_models.py
@@ -10,3 +10,9 @@ class UtilityMatrixTest(SimpleTestCase):
         sparse_array = dok_matrix((10, 100), dtype=np.int8)
         serialized_array = convert_from_sparse_array(sparse_array)
         self.assertIsNotNone(serialized_array)
+
+    def test_utility_matrix_empty_array_return_an_empty_string(self):
+        expected_serialized_array = ''
+        sparse_array = dok_matrix((1, 100), dtype=np.int8)
+        serialized_array = convert_from_sparse_array(sparse_array)
+        self.assertEqual(serialized_array, expected_serialized_array)
