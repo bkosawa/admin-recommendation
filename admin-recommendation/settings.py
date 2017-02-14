f6427e250c64f05b7de6c8807d3c5c1017de04d5
bruno.kosawa <bkosawa@gmail.com>
2017-02-14 09:55:38 -0200

    Adding setting to only admin could make request

--- admin-recommendation/settings.py
+++ admin-recommendation/settings.py
@@ -45,6 +45,13 @@ INSTALLED_APPS = [
     'crawler',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
+
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
