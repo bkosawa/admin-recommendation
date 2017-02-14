1dcf35048dfd991530171f957a60ec504d606a25
bruno.kosawa <bkosawa@gmail.com>
2017-02-14 09:29:32 -0200

    Creating app view to return serialized app

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from crawler.models import App
+from crawler.serializers import AppSerializer
+
+
+class AppList(APIView):
+    def get(self, request, format=None):
+        users = App.objects.all()[:25]
+        serializer = AppSerializer(users, many=True)
+        return Response(serializer.data)
