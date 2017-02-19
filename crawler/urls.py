bd9f7b810ce47a2e9f52dfe5f4bcf48ae42a492b
bruno.kosawa <bkosawa@gmail.com>
2017-02-19 11:39:10 -0300

    Adding auth token endpoint

--- crawler/urls.py
+++ crawler/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url, include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from crawler import views
 
@@ -8,5 +9,5 @@ router.register(r'apps', views.AppViewSet)
 
 urlpatterns = patterns('',
                        url(r'^', include(router.urls)),
-                       url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+                       url(r'^api-token-auth/', obtain_auth_token)
                        )
