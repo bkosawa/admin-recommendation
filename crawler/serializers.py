9c1311e4a2008b7c09a544553e9df4ed703b139b
Bruno Kosawa <bkosawa@gmail.com>
2017-04-02 14:46:40 -0300

    Adding category name in app serializer

--- crawler/serializers.py
+++ crawler/serializers.py
@@ -6,7 +6,7 @@ from crawler.models import App, UserApps
 class AppSerializer(serializers.ModelSerializer):
     class Meta:
         model = App
-        fields = ('id', 'package_name', 'name', 'icon_url', 'category_key', 'developer_name')
+        fields = ('id', 'package_name', 'name', 'icon_url', 'category_key', 'category_name', 'developer_name')
 
 
 class UserAppsSerializer(serializers.ModelSerializer):
