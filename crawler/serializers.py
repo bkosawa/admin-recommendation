0afe0e526cd59ac02db4e4265f6edc934eb7363d
Bruno Kosawa <bkosawa@gmail.com>
2017-04-02 14:47:29 -0300

    Adding serializer to return full app data

--- crawler/serializers.py
+++ crawler/serializers.py
@@ -9,6 +9,13 @@ class AppSerializer(serializers.ModelSerializer):
         fields = ('id', 'package_name', 'name', 'icon_url', 'category_key', 'category_name', 'developer_name')
 
 
+class FullAppSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = App
+        fields = ('id', 'package_name', 'name', 'description', 'icon_url', 'category_key', 'category_name',
+                  'developer_name', 'version', 'content_rating', 'size')
+
+
 class UserAppsSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserApps
