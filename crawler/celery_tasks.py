d8dbe17eeb4fe40c453dfac41eeda43af9a45cf2
Bruno Kosawa <bkosawa@gmail.com>
2017-05-16 21:14:10 -0300

    When recommending not removing apps that user already has installed

--- crawler/celery_tasks.py
+++ crawler/celery_tasks.py
@@ -28,20 +28,18 @@ def recommend_to_user(user_id):
         similar_apps = SimilarApp.objects.filter(source_package=package_name).all()
         for similar_app in similar_apps:
             similar_app_package = similar_app.similar_package
-            if not UserApps.objects.filter(package_name=similar_app_package).exists():
-                app_similar = App.objects.filter(package_name=similar_app_package).first()
-                if app_similar:
-                    logger.info("Similar to {} found: {}".format(package_name, app_similar.package_name))
-                    apps.add(app_similar)
+            app_similar = App.objects.filter(package_name=similar_app_package).first()
+            if app_similar:
+                logger.info("Similar to {} found: {}".format(package_name, app_similar.package_name))
+                apps.add(app_similar)
 
         other_similar_apps = SimilarApp.objects.filter(similar_package=package_name).all()
         for similar_app in other_similar_apps:
             similar_app_package = similar_app.similar_package
-            if not UserApps.objects.filter(package_name=similar_app_package).exists():
-                app_similar = App.objects.filter(package_name=similar_app_package).first()
-                if app_similar:
-                    logger.info("Similar to {} found: {}".format(package_name, app_similar.package_name))
-                    apps.add(app_similar)
+            app_similar = App.objects.filter(package_name=similar_app_package).first()
+            if app_similar:
+                logger.info("Similar to {} found: {}".format(package_name, app_similar.package_name))
+                apps.add(app_similar)
 
     if len(apps) > 0:
         user.recommended_apps = list(apps)
